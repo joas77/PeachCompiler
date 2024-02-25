808613ff37972738ddc6f8106d216b40c85c0aeb
Julio Angulo <solojoas@gmail.com>
2024-02-25 13:33:57 +0000

    Starting to specify tokens

--- include/compiler.hpp
+++ include/compiler.hpp
@@ -2,8 +2,39 @@
 #define PEACHCOMPILER_HPP
 
 #include <filesystem>
+#include <variant>
 
 namespace fs = std::filesystem;
+
+struct Position {
+    int line;
+    int column;
+    fs::path filename;
+};
+
+enum class TokenType{
+    kIdentifier,
+    kKeyword,
+    kOperator,
+    kSymbol,
+    kNumber,
+    kString,
+    kComment,
+    kNewLine
+};
+
+struct Token {
+    TokenType type;
+    int flags;
+
+    //TODO: specify the value that holds the token as std::variant
+    //std::variant<char> value;
+    // True if there is a whitespace between the token and the next token
+    // i.e * a for operator token * would mean whitespace would be set for token "a"
+    bool whitespace;
+
+    std::string between_brackets;
+};
 
 enum class CompilerCodes{
     kCompiledOk,
@@ -15,7 +46,7 @@ public:
     CompilerCodes CompileFile(const fs::path& filename, const fs::path& out_file, int flags);
 private:
     // The flags regards how this file should be compiled
-    int flags;
+    int flags_;
 };
 
 #endif //PEACHCOMPILER_HPP
